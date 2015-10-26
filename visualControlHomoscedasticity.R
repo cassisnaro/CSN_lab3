1d8a407ed4bca4c54d322e7c87e4c4ed78049c92
cassisnaro <narowirf@gmail.com>
2015-10-26 23:31:12 +0100

    trying to include one model more.

--- visualControlHomoscedasticity.R
+++ visualControlHomoscedasticity.R
@@ -1,4 +1,4 @@
-numberbins <- 50;
+numberbins <- 5;
 
 source = read.table("list.txt", 
                     header = TRUE,               # this is to indicate the first line of the file contains the names of the columns instead of the real data
@@ -7,20 +7,40 @@ source = read.table("list.txt",
 
 for(x in 1:nrow(source)){
   dataRead = read.table(source$file[x],header=FALSE)
+  colnames(dataRead) = c("vertices","degree_2nd_moment","mean_length")
   language <- source$language[x]
   
 
   vars <- array(dim=c(numberbins))
   means <- array(dim=c(numberbins))
-  dataRead$group <- as.numeric(cut(dataRead$V1, numberbins))
-  postscript(paste('./homoscedasticity/',language,"_noModel",'.ps',sep = ""))
-  plot(range(dataRead$V1), range(0,5),type="n")
+  dataRead$group <- as.numeric(cut(dataRead$vertices, numberbins))
   for (i in 1:numberbins){
     dataToProcess <- dataRead[which (dataRead$group==i),]
-    means[i] <- mean(dataToProcess$V1)
-    vars[i] <- var(dataToProcess$V2)
-    segments(min(dataToProcess$V1),vars[i],max(dataToProcess$V1,vars[i]))
+    means[i] <- mean(dataToProcess$vertices)
+    vars[i] <- var(dataToProcess$degree_2nd_moment)
   }
+  postscript(paste('./homoscedasticity/',language,"_noModel",'.ps',sep = ""))
+  plot(range(dataRead$vertices), range(vars),type="n")
+  for (i in 1:numberbins){
+    dataToProcess <- dataRead[which (dataRead$group==i),]
+    segments(min(dataToProcess$vertices),vars[i],max(dataToProcess$vertices,vars[i]))
+  }  
   dev.off()
+  
+
+  if (x==10){
+    new_vars <- array(dim=c(numberbins))
+    for (i in 1:numberbins){
+      predicted_value <- (means[i]/2)^0.6736349;
+      new_vars[i] <- var(dataToProcess$degree_2nd_moment - predicted_value);
+    }
+    postscript(paste('./homoscedasticity/',language,"_model1",'.ps',sep = ""))
+    plot(range(dataRead$vertices), range(vars),type="n")
+    for (i in 1:numberbins){
+      dataToProcess <- dataRead[which (dataRead$group==i),]
+      segments(min(dataToProcess$vertices),new_vars[i],max(dataToProcess$vertices,new_vars[i]))
+    }  
+    dev.off()
+  }
 }
 
